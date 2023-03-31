79058bbe3fc31ac111998543597179fff2ed6ed2
nicolaskawer <nicolaskawer@gmail.com>
2023-03-31 15:57:55 +0300

    test1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const port = process.env.PORT || 4000;
+const app = express();
+app.get('/', (req, res) => {
+  res.send('<h2>Hi from server main page<h2>');
+});
+app.listen(port, () => {
+  console.log('server is up and running');
+});
