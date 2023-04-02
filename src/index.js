8b9207d3a3078fe6f2415f92c0cd9725838906ae
nicolaskawer <nicolaskawer@gmail.com>
2023-04-02 15:09:40 +0300

    Change end of line charachter (\r\n)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const port = process.env.PORT || 4000;
 const app = express();
 app.get('/', (req, res) => {
-    res.send('<h2>Hi from server main page<h2>');
+    res.send('<h2>Hi from server main page 2<h2>');
 });
 app.listen(port, () => {
     // eslint-disable-next-line no-console
