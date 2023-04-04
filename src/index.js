dd0eea5d111ef9f6efc53e9a1e13646ac79422ec
nicolaskawer <nicolaskawer@gmail.com>
2023-04-04 16:09:33 +0300

    ver_last_promise

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const port = process.env.PORT || 4000;
 const app = express();
 app.get('/', (req, res) => {
-    res.send('<h2>Hi from server main page<h2> ');
+    console.log('Got a request');
+    res.send(JSON.stringify('<h3>Hi From Backend API</h3>'));
     return res;
 });
 app.listen(port, () => {
