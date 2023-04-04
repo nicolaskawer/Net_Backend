86feb2566cc828ca0d1cf65edf33b3518607571a
nicolaskawer <nicolaskawer@gmail.com>
2023-04-04 16:07:46 +0300

    ver_last_changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 4000;
 const app = express();
 app.get('/', (req, res) => {
     res.send('<h2>Hi from server main page<h2> ');
+    return res;
 });
 app.listen(port, () => {
     // eslint-disable-next-line no-console
