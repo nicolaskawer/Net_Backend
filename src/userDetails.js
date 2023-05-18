6370c40730fdb8ddb1a05a2749a6101c22bbc52d
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2023-05-18 23:31:47 +0300

    mongotry1

---
+++ src/userDetails.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: true
+  },
+  lname: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  birthdate: {
+    type: Date,
+    required: true
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
