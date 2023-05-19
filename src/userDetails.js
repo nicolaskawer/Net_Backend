7427e9fae2f446f44a79ed641e1f90b15f10fdd9
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2023-05-19 17:26:14 +0300

    linterfix2

--- src/userDetails.js
+++ src/userDetails.js
@@ -1,31 +1,31 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  birthdate: {
-    type: String,
-    required: true,
-  },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    birthdate: {
+        type: String,
+        required: true,
+    },
 });
 
 module.exports = mongoose.model("users", userSchema);
